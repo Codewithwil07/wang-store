b36705119108da1075c342ed3442cfc5fc313cbf
wilywangceo@gmail.com <wilywangceo@gmail.com>
2024-06-08 20:51:23 +0700

    category manage

--- wangstore/backend/app.js
+++ wangstore/backend/app.js
@@ -7,9 +7,10 @@ import cookieParser from 'cookie-parser';
 // utils
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoues.js';
+import categoryRoutes from './routes/categoryRoutes.js';
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
@@ -17,10 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-
-
-
 app.use('/api/users', userRoutes);
+app.use('/api/category', categoryRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
