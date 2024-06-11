b85292abaf6b0465f5cba640b6a7705c38de51d0
wilywangceo@gmail.com <wilywangceo@gmail.com>
2024-06-11 21:08:32 +0700

    create category

--- wangstore/backend/controllers/categoryController.js
+++ wangstore/backend/controllers/categoryController.js
@@ -4,7 +4,19 @@ import asyncHandler from '../middlewares/asyncHandler.js';
 const createCategory = asyncHandler(async (req, res) => {
   try {
     const { name } = req.body;
-    console.log(name);
+
+    if (!name) {
+      return res.json({ error: 'Name is required' });
+    }
+
+    const existingCategory = await Category.findOne({ name });
+
+    if (existingCategory) {
+      return res.json({ error: 'Already exists' });
+    }
+
+    const category = await new Category({ name }).save();
+    res.json(category)
   } catch (error) {
     console.log(error);
     return res.status(400).json(error);
