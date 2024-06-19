073a2f1b50133925b53f8c7d80dbe0c4afa9e9a3
wilywangceo@gmail.com <wilywangceo@gmail.com>
2024-06-19 21:29:21 +0700

    add product manage

---
+++ wangstore/backend/controllers/productController.js
@@ -0,0 +1,60 @@
+import asyncHandler from '../middlewares/asyncHandler.js';
+import Product from '../models/productModel.js';
+
+const addProduct = asyncHandler(async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, brand } = req.fields;
+    switch (true) {
+      case !name:
+        return res.json({ error: 'Name is required' });
+      case !description:
+        return res.json({ error: 'Description is required' });
+      case !price:
+        return res.json({ error: 'Price is required' });
+      case !category:
+        return res.json({ error: 'Category is required' });
+      case !quantity:
+        return res.json({ error: 'Quantity is required' });
+      case !brand:
+    }
+
+    const product = new Product({ ...req.fields });
+    await product.save();
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json(error.message);
+  }
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, brand } = req.fields;
+    switch (true) {
+      case !name:
+        return res.json({ error: 'Name is required' });
+      case !description:
+        return res.json({ error: 'Description is required' });
+      case !price:
+        return res.json({ error: 'Price is required' });
+      case !category:
+        return res.json({ error: 'Category is required' });
+      case !quantity:
+        return res.json({ error: 'Quantity is required' });
+      case !brand:
+    }
+
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      { ...req.fields },
+      { new: true }
+    );
+    await product.save();
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json(error.message);
+  }
+});
+
+export { addProduct, updateProduct };
